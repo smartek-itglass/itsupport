29ac58b50af0a16bbc9fbd192dbadd19a311e889
romeshsahu <romesh.sahu@parkhya.com>
2017-05-03 17:03:32 +0530

    committed on 03-05-17

--- itsupport/itsupport/application/views/admin/footer.php
+++ itsupport/itsupport/application/views/admin/footer.php
@@ -1,5 +1,5 @@
 
 <div class="footer">
-    <div class="footer-inner"> 2015 &copy; Fooder App by Parkhya Solution Pvt. Ltd.. </div>
+    <div class="footer-inner"> 2015 &copy; IT SUPPORT by Parkhya Solution Pvt. Ltd.. </div>
     <div class="footer-tools"><span class="go-top"><i class="fa fa-angle-up"></i></span></div>
 </div>
